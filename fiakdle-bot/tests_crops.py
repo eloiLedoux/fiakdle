a37e3723ff753c3b1d5d7e6401f33c2c95f2e149
eloiLedoux <eloiledoux@hotmail.fr>
2024-03-17 16:52:50 +0100

    feat : test pour verifier les bon découpage des images

---
+++ fiakdle-bot/tests_crops.py
@@ -0,0 +1,32 @@
+from construire_info import construire_fiak
+from cropper import crop_image
+
+NB_IMAGES_BDD = 38
+
+if __name__ == "__main__":
+
+    for id in range(1,NB_IMAGES_BDD+1):
+        img_save_url = "tests_images/f"+str(id)+"_z"
+        print(img_save_url)
+        fiaks_totest = construire_fiak(id)
+        fiaks_totest.augmenterAide()
+
+        img_save_url_0 = img_save_url+str(fiaks_totest.getAide())+".jpg"
+        print(img_save_url_0)
+        crop_image(fiaks_totest.getImgUrl(), img_save_url_0, fiaks_totest.getZoom())
+        fiaks_totest.augmenterAide()
+        
+        img_save_url_1 = img_save_url+str(fiaks_totest.getAide())+".jpg"
+        print(img_save_url_1)
+        crop_image(fiaks_totest.getImgUrl(), img_save_url_1, fiaks_totest.getZoom())
+        fiaks_totest.augmenterAide()
+        
+        img_save_url_2 = img_save_url+str(fiaks_totest.getAide())+".jpg"
+        print(img_save_url_2)
+        crop_image(fiaks_totest.getImgUrl(), img_save_url_2, fiaks_totest.getZoom())
+        fiaks_totest.augmenterAide()
+
+        img_save_url_3 = img_save_url+str(fiaks_totest.getAide())+".jpg"
+        print(img_save_url_3)
+        crop_image(fiaks_totest.getImgUrl(), img_save_url_3, fiaks_totest.getZoom())
+        fiaks_totest.augmenterAide()
