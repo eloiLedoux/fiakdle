75d7ae72b33b6418ae10be0d45c8b3f72f9dc115
tom <tomletourneur1867@gmail.com>
2025-05-20 22:38:22 +0200

    ajout d'une animation de reussite et d'une liste de tentatives

---
+++ Angular/src/app/errorframe/errorframe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-errorframe',
+  imports: [],
+  templateUrl: './errorframe.component.html',
+  styleUrl: './errorframe.component.scss'
+})
+export class ErrorframeComponent {
+
+}
--- Angular/src/app/guessframe/guessframe.component.ts
+++ Angular/src/app/guessframe/guessframe.component.ts
@@ -9,35 +9,40 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { ApiService } from '../api.service';
 import { Perso } from '../api.service';
-import { rollInAnimation, shakeAnimation, bounceAnimation, rubberBandAnimation, collapseAnimation } from "angular-animations";
+import { shakeAnimation, tadaAnimation } from "angular-animations";
 
 
 
 @Component({
   selector: 'app-guessframe',
-  imports: [FormsModule, ReactiveFormsModule, MatAutocompleteModule, MatInputModule, MatFormFieldModule, MatSlideToggleModule, AsyncPipe],
+  imports: [ FormsModule, ReactiveFormsModule, MatAutocompleteModule, MatInputModule, MatFormFieldModule, MatSlideToggleModule, AsyncPipe],
   templateUrl: './guessframe.component.html',
   styleUrls: ['./guessframe.component.scss'],
   animations: [
-    bounceAnimation({ duration: 2000 }),
-    rubberBandAnimation({anchor: 'rubber', direction: '=>', duration: 500}),
-    collapseAnimation(),
+    tadaAnimation(),
     shakeAnimation(),
-    rollInAnimation()
   ]
 })
 export class GuessframeComponent {
-  persoCtrl = new FormControl('');
+  persoCtrl = new FormControl<string | Perso>('');
   filteredPerso: Observable<Perso[]>;
   liste: Perso[];
   image : string;
-  animState = false;
+  errorAnimation = false;
+  winAnimation = false;
+  win=false;
+  backgroundColor = "text-bg-dark";
+
+  errorListe : number[] = []
 
   constructor(private apiService: ApiService) {
     this.liste = this.apiService.getList();
     this.filteredPerso = this.persoCtrl.valueChanges.pipe(
       startWith(''),
-      map(str => (str ? this._filter(str) : [])),
+      map(value => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : [];
+      }),
     );
     this.image = this.apiService.getImageOfTheDay();
   }
@@ -47,20 +52,55 @@ export class GuessframeComponent {
     if(value.length <2){
       return [];
     }
-    return this.liste.filter(state => (state.name.toLowerCase().includes(filterValue) || state.manga.toLowerCase().includes(filterValue)));
+    return this.liste.filter(perso => (perso.name.toLowerCase().includes(filterValue) || perso.manga.toLowerCase().includes(filterValue)));
   }
 
   onSubmit() {
-    this.animState = true;
     if(this.persoCtrl.value === null){
       return;
     }
-    var response = this.apiService.guess(this.persoCtrl.value);
-    
-    if(response == true){
-      console.log("gg");
+    if( typeof this.persoCtrl.value === 'string'){
+      return;
     }
+    console.log(this.persoCtrl.value);
+    
+    var indexPerso = -1;
+    for (let index = 0; index < this.liste.length; index++) {
+      if(this.liste[index].name.toLowerCase() == this.persoCtrl.value.name.toString().toLowerCase()){
+        indexPerso = index;
+        break;
+      }
+    }
+    if(indexPerso == -1){
+      return;
+    }
+
+    var response = this.apiService.guess(this.persoCtrl.value.name);
+
+    if(response == true){
+      this.backgroundColor = "text-bg-success";
+      this.winAnimation = !this.winAnimation;
+      this.win=true;
+
+    }else{
+      this.errorAnimation = !this.errorAnimation;
+      
+      if (this.errorListe.length >=12){
+        this.errorListe.pop()
+      }
+      this.errorListe.unshift(indexPerso);
+      
+      
+    }
+
     this.persoCtrl.reset();
+  }
+
+  displayFonction(perso: Perso){
+    if(perso === null){
+      return '';
+    }
+    return perso.name;
   }
 
 }
